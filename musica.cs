ec809f059ff2382e40b98183b42febc31b2573ff
brandluiza <karlaluizaalvessilva@gmail.com>
2025-03-07 22:16:50 -0300

    Add files via upload

---
+++ musica.cs
@@ -0,0 +1,80 @@
+﻿// Limpar a tela
+Console.Clear();
+// MUdar cor da letra
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.White;
+// Exibir a letra da música "All that Matters"
+Console.WriteLine("All That Matters - Justin Bieber");
+Console.ResetColor();
+Console.WriteLine();
+
+// Quebra de linha
+Console.Write("\n---\n---\n");
+
+Console.WriteLine(@" Oh, oh
+Just as sure as the stars in the sky
+I need you to shine in my life
+Not just for the meanwhile, for a long, long time
+Better believe it
+
+Oh, oh
+Whenever you're not in my presence
+It feels like I'm missing my blessings, yeah
+So I sleep through the daylight, stay awake all night
+You think I'm biased to my significant other
+You hit it right on the head, only been missing my lover
+Got a whole lot of texts in my phone and I don't reply
+But the next eight bars tell you why");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"
+You're all that matters to me
+Yeah, yeah
+Ain't worried about nobody else
+If I ain't with you, I ain't myself
+You make me complete
+
+You're all that matters to me
+Yeah, yeah
+What's a king, babe, without a queen?
+There ain't no I in team, you make me complete
+You're all that matters to me");
+
+Console.ResetColor();
+Console.WriteLine(@"
+Take the gas out the car, it won't drive
+That's how I feel when you're not by my side
+When I wake up in the morning up under you
+And only you
+
+Oh, oh
+I'm grateful for your existence
+Faithful no matter the distance
+You're the only girl I see
+From the bottom of my heart, please, believe");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"
+You're all that matters to me
+Yeah, yeah
+Ain't worried about nobody else
+If I ain't with you, I ain't myself
+You make me complete
+
+You're all that matters to me
+Yeah, yeah
+What's a king, babe, without a queen?
+There ain't no I in team, you make me complete
+You're all that matters to me");
+
+Console.ResetColor();
+Console.WriteLine(@"
+Yeah, yeah
+Yeah, yeah
+You're all that matters to me
+
+Yeah, yeah
+Yeah, yeah
+You're all that matters to me
+
+ ");
